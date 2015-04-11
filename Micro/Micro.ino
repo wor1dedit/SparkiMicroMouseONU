76a4dd6f4261d332aa0f585dec592532e9b8e140
BuildTools <unconfigured@null.spigotmc.org>
2015-04-11 11:38:27 -0400

    Added Micro.ino
    This is a left hand solver and a right hand solver.

---
+++ Micro/Micro.ino
@@ -0,0 +1,220 @@
+#include <Sparki.h>
+#include <DistanceGP2Y0A41SK.h>
+
+DistanceGP2Y0A41SK irRight;
+DistanceGP2Y0A41SK irLeft;
+
+const int RIGHT_IR_PIN = A8;
+const int LEFT_IR_PIN = A1;
+
+int rightDistance = 0;
+int leftDistance = 0;
+int centerDistance = 0;
+
+void setup()
+{
+  irRight.begin(RIGHT_IR_PIN);
+  irLeft.begin(LEFT_IR_PIN);
+}
+
+void loop()
+{
+    leftHandSolver();
+}
+
+void leftHandSolver()
+{
+  while(true)
+  {
+    if(centerDistance < 18 && centerDistance > 8)
+    {
+      sparki.moveBackward(centerDistance - 2);
+    }
+    else if(centerDistance > 18)
+    {
+      sparki.moveBackward(18)
+    }
+    centerDistance = sparki.ping();
+    rightDistance = irRight.getDistanceCentimeter();
+    leftDistance = irLeft.getDistanceCentimeter();
+    delay(100); // wait 0.1 seconds (100 milliseconds)
+    if(centerDistance < 8 || leftDistance > 8)
+    {
+      if(leftDistance > 8)
+      {
+        sparki.moveRight(90);
+      }
+      else if(rightDistance > 8)
+      {
+        sparki.moveLeft(90);
+      }
+      else if(rightDistance > leftDistance)
+      {
+        sparki.moveLeft(180);
+      }
+      else
+      {
+        sparki.moveRight(180);
+      }
+    }
+  }
+}
+
+void rightHandSolver()
+{
+  while(true)
+  {
+    if(centerDistance < 18 && centerDistance > 8)
+    {
+      sparki.moveBackward(centerDistance - 2);
+    }
+    else if(centerDistance > 18)
+    {
+      sparki.moveBackward(18)
+    }
+    centerDistance = sparki.ping();
+    rightDistance = irRight.getDistanceCentimeter();
+    leftDistance = irLeft.getDistanceCentimeter();
+    delay(100); // wait 0.1 seconds (100 milliseconds)
+    if(centerDistance < 8 || rightDistance > 8)
+    {
+      if(rightDistance > 8)
+      {
+        sparki.moveLeft(90);
+      }
+      else if(leftDistance > 8)
+      {
+        sparki.moveRight(90);
+      }
+      else if(rightDistance > leftDistance)
+      {
+        sparki.moveLeft(180);
+      }
+      else
+      {
+        sparki.moveRight(180);
+      }
+    }
+  }
+}
+
+void randomSolver()
+{
+  while(true)
+  {
+    centerDistance = sparki.ping();
+    rightDistance = irRight.getDistanceCentimeter();
+    leftDistance = irLeft.getDistanceCentimeter();
+    delay(100); // wait 0.1 seconds (100 milliseconds)
+    
+    bool skip = false;
+    int dist = 0;
+    int turnDist = 12; 
+    int lastWallLeft = 0;
+    int lastWallRight = 0;
+    
+    while(centerDistance > 11)
+    {
+      sparki.moveBackward(6);
+      dist = dist + 6;
+      
+      rightDistance = irRight.getDistanceCentimeter();
+      leftDistance = irLeft.getDistanceCentimeter();
+      
+      int rand = random(0,6);
+      
+      if(leftDistance >= 10)
+      {
+        lastWallLeft += 6;
+        if(rand == 5)
+        {
+          sparki.moveBackward(turnDist - lastWallLeft);
+          sparki.moveRight(90); 
+          skip = true;
+          break;
+        }
+      }
+
+      if(rightDistance >= 10)
+      {
+        lastWallRight += 6;
+        if(rand == 4)
+        {
+          sparki.moveBackward(turnDist - lastWallRight);
+          sparki.moveLeft(90);
+          skip = true;
+          break;
+        }
+      }
+
+      if(leftDistance < 10)
+      {
+        lastWallLeft = 0;
+      }
+
+      if(rightDistance < 10)
+      {
+        lastWallRight = 0;
+      }
+      
+      lastWallRight = (lastWallRight >= 18) ? 0 : lastWallRight;
+      lastWallLeft = (lastWallLeft >= 18) ? 0 : lastWallLeft;
+      
+      delay(125);
+      
+      centerDistance = sparki.ping();
+    }
+  
+    sparki.moveStop();
+
+    if(!skip)
+    {
+      sparki.moveBackward(centerDistance - 3);
+      
+      rightDistance = irRight.getDistanceCentimeter();
+      leftDistance = irLeft.getDistanceCentimeter();
+
+      while(true)
+      {
+        if(leftDistance == -1)
+        {
+          leftDistance = irLeft.getDistanceCentimeter();
+        }
+        else if(rightDistance == -1)
+        {
+          rightDistance = irRight.getDistanceCentimeter();
+        }
+        else if(leftDistance == rightDistance) 
+        {
+          rightDistance = irRight.getDistanceCentimeter();
+          leftDistance = irLeft.getDistanceCentimeter();
+        }
+        if(leftDistance < 5 && rightDistance < 5)
+        {
+          rightDistance = irRight.getDistanceCentimeter();
+          leftDistance = irLeft.getDistanceCentimeter();
+          if(leftDistance > 5 || rightDistance > 5)
+          {
+            continue;
+          }
+          else
+          {
+            sparki.moveLeft(180);
+            break;
+          }
+        }
+        else if(leftDistance > rightDistance)
+        {
+          sparki.moveLeft(90);
+          break;
+        }
+        else
+        {
+          sparki.moveRight(90);
+          break;
+        }
+      }
+      sparki.moveStop();
+    }
+  }
+}
--- MicroMouse/MicroMouse.ino
+++ MicroMouse/MicroMouse.ino
@@ -1,10 +1,16 @@
 //Libraries
-#include <Sparki.h> // include the sparki library
-#include <DistanceGP2Y0A41SK.h>
+//#include <Sparki.h> // include the sparki library
+#include "Sparki.h"
+#include "DistanceGP2Y0A41SK.h"
+#include "Cell.h"
+#include "CellStack.h"
 
 //Distance Sensors
-DistanceGP2Y0A41SK DistRight;
-DistanceGP2Y0A41SK DistLeft;
+DistanceGP2Y0A41SK irRight;
+DistanceGP2Y0A41SK irLeft;
+byte leftIR;
+byte rightIR;
+int forwardDistance;
 
 const int RIGHT_IR_PIN = A8;
 const int LEFT_IR_PIN = A1;
@@ -13,109 +19,366 @@ int rightDistance = 0;
 int leftDistance = 0;
 int centerDistance = 0;
 
+#define WALL_THRESHOLD_FORWARD 5 //cm from the the front sensor to up to
+#define WALL_THRESHOLD_LEFT 7
+#define WALL_THRESHOLD_RIGHT 7
+#define TURN_NUMBER_LEFT 14 //number the encoder should check when turning left
+#define TURN_NUMBER_RIGHT -17 //number the encoder should check when turning right
+#define TURN_SPEED  30 //speed for turning
+#define TURN_CORRECTION 30 //speed to turn one wheel on correction
+#define TURN_CORRECTION_DELAY 50 //delay to turn for error in milliseconds
+#define LEFT 0
+#define RIGHT 1
+#define BEHIND 2
+int LEFT_ENCODER_TICKS_FORWARD;
+int RIGHT_ENCODER_TICKS_FORWARD;
+int leftEncoder;
+int rightEncoder;
+
+//check distance is to know if the wall is in the cell or not
+#define CHECK_DISTANCE_FORWARD 12 //centimeters forward
+#define CHECK_DISTANCE_LEFT 15
+#define CHECK_DISTANCE_RIGHT 15
+
+#define ISNORTH 1   //0b00000001
+#define ISEAST 2    //0b00000010
+#define ISSOUTH 4   //0b00000100
+#define ISWEST 8    //0b00001000
+#define ISEXPLORED 16 //0b00010000
+#define NOTEXPLORED 239 //0b11101111
+#define Y_ONLY 15   //0b00001111
+#define SHIFT 4 //shift 4 bits
+
+#define MAZE_LENGTH 12
+Cell maze[MAZE_LENGTH][MAZE_LENGTH];
+Cell* currentCell;
+CellStack stack;
+byte currentDirection;
+#define USELESS 255
+
+
 void setup()
 {
   sparki.clearLCD();
-  DistRight.begin(RIGHT_IR_PIN);
-  DistLeft.begin(LEFT_IR_PIN);
-  Serial.begin(9600);
+  irRight.begin(RIGHT_IR_PIN);
+  irLeft.begin(LEFT_IR_PIN);
+  Serial.begin (9600);
+  
+  for (byte y = 0; y < MAZE_LENGTH; y++)
+  {
+    for(byte x = 0; x < MAZE_LENGTH; x++)
+    {
+    
+      maze[x][y].position = (x << SHIFT); //x position
+      maze[x][y].position |= (y);//y position
+      maze[x][y].data = 0x00;
+      
+    }
+  }
+  //fill the distances
+  byte half = MAZE_LENGTH / 2;
+  for (byte y = 0; y < half; y++)
+  {
+    for (byte x = 0; x < half; x++)
+    {
+      byte m = MAZE_LENGTH - 2 - x - y;
+      maze[x][y].distance = m;
+      maze[MAZE_LENGTH - 1 - x][y].distance = m;
+      maze[MAZE_LENGTH - 1 - x][MAZE_LENGTH - 1 - y].distance = m;
+      maze[x][MAZE_LENGTH - 1 - y].distance = m;
+    }
+  }
+  printMap();
+  currentDirection = ISNORTH;
+  currentCell = &maze[0][0];
+  currentCell->data |= ISEAST;
+  currentCell->data |= ISWEST;
+  currentCell->data |= ISSOUTH;
+  currentCell->data |= ISEXPLORED;
+  stack.push(currentCell);
+  randomSeed(analogRead(0));
 }
 
 void loop()
 {
-    centerDistance = sparki.ping();
-    rightDistance = DistRight.getDistanceCentimeter();
-    leftDistance = DistLeft.getDistanceCentimeter();
-    delay(100); // wait 0.1 seconds (100 milliseconds)
-    /*
-    while(true)
-    {
-          centerDistance = sparki.ping();
-          rightDistance = DistRight.getDistanceCentimeter();
-          leftDistance = DistLeft.getDistanceCentimeter();
-          Serial.print("Left: ");
-          Serial.println(leftDistance);
-          Serial.print("Center: ");
-          Serial.println(centerDistance);
-          Serial.print("Right: ");
-          Serial.println(rightDistance);
-          Serial.println("- - - - - - - - - - -");
-          delay(500);
-    }*/
+  while (currentCell->distance == 0);
+  updateWalls();
+  byte moveDirection = getBestDirection();
+  move(moveDirection); 
+}
 
-    sparki.moveBackward();
-    while(centerDistance > 11)
+void turn(int direction)
+{
+  if(direction == LEFT)
+  {
+    sparki.moveRight(90);
+  }
+  else
+  {
+    sparki.moveLeft(90);
+  }
+
+  delay(500);
+  currentDirection = getDirection(direction);
+}
+
+
+void move(byte direction)
+{
+  if (currentDirection != direction)
+  {
+    if (getDirection(LEFT) == direction)
     {
-      rightDistance = DistRight.getDistanceCentimeter();
-      leftDistance = DistLeft.getDistanceCentimeter();
-      
-      if(leftDistance < 4)
-      {
-        sparki.moveRight(1);
-      }
-      if(rightDistance < 4)
-      {
-        sparki.moveLeft(1);
-      }
-      sparki.moveBackward();
-      delay(125);
-      centerDistance = sparki.ping();
+      turn(LEFT);
     }
-
-    
-    /*while(centerDistance > 4)
+    else if (getDirection(RIGHT) == direction)
     {
-      sparki.moveBackward();
-      centerDistance = sparki.ping();
-      delay(50);
-    }*/
+      turn(RIGHT);
+    }
+    else
+    {
+      //turn around
+      turn(LEFT);
+      turn(LEFT);
+    }
+  }
+  forwardDistance = getSonarDistance();
+  sparki.moveBackward(18);
   
-    sparki.moveStop();
-    sparki.moveBackward(9);
-    
-    rightDistance = DistRight.getDistanceCentimeter();
-    leftDistance = DistLeft.getDistanceCentimeter();
+  sparki.moveStop();
 
-        
-    while(true)
+  byte x = (currentCell->position >> SHIFT);
+  byte y = (currentCell->position & Y_ONLY);
+  switch (currentDirection)
+  {
+    case ISNORTH:
+      y++;
+      break;
+    case ISEAST:
+      x++;      
+      break;
+    case ISSOUTH:
+      y--;
+      break;
+    case ISWEST:
+      x--;
+      break;
+  }
+  currentCell = &maze[x][y];
+  if (stack.contains(currentCell))
+  {
+    Cell* temp;
+    //keep popping cell off stack untill it finds the cell and put it back on
+    //this gets rid of the unneccessary cells like in a dead end
+    //I am pretty sure I spelled ^ wrong
+    while (stack.pop() != currentCell);
+  }
+  currentCell->data |= ISEXPLORED;
+  stack.push(currentCell);
+}
+
+int getSonarDistance()
+{
+  return sparki.ping();
+}
+
+void printMap()
+{
+  for (int i = 0; i < MAZE_LENGTH; i++)
+  {
+    for (int j = 0; j < MAZE_LENGTH; j++)
     {
-      if(leftDistance == -1)
-      {
-        leftDistance = DistLeft.getDistanceCentimeter();
-      }
-      else if(rightDistance == -1)
-      {
-        rightDistance = DistRight.getDistanceCentimeter();
-      }
-      else if(leftDistance == rightDistance) 
-      {
-        rightDistance = DistRight.getDistanceCentimeter();
-        leftDistance = DistLeft.getDistanceCentimeter();
-      }
-      if(leftDistance < 5 && rightDistance < 5)
-      {
-        rightDistance = DistRight.getDistanceCentimeter();
-        leftDistance = DistLeft.getDistanceCentimeter();
-        if(leftDistance > 5 || rightDistance > 5)
-        {
-          continue;
-        }
-        else
-        {
-          sparki.moveLeft(180);
-          break;
-        }
-      }
-      else if(leftDistance > rightDistance)
-      {
-        sparki.moveLeft(90);
-        break;
-      }
-      else
-      {
-        sparki.moveRight(90);
-        break;
-      }
+      Serial.print(maze[i][j].distance);
+      Serial.print(" ");
     }
-    sparki.moveStop();
+    Serial.println("");
+  }
+}
+
+void updateWalls()
+{
+  if (getSonarDistance() <= WALL_THRESHOLD_FORWARD)
+  {
+    updateWall(currentDirection);
+  }
+  if (irLeft.getDistanceCentimeter() <= WALL_THRESHOLD_LEFT)
+  {
+    updateWall(getDirection(LEFT));
+  }
+  if (irRight.getDistanceCentimeter() <= WALL_THRESHOLD_RIGHT)
+  {
+    updateWall(getDirection(RIGHT));
+  }
+}
+
+void updateWall(byte direction)
+{
+  currentCell->data |= direction;
+}
+
+byte getDirection(byte direction)
+{
+  if (direction == LEFT)
+  {
+    switch (currentDirection)
+    {
+      case ISNORTH:
+        return ISWEST;
+      case ISEAST:
+        return ISNORTH;
+      case ISSOUTH:
+        return ISEAST;
+      case ISWEST:
+        return ISSOUTH;
+    }
+  }
+  else if (direction == RIGHT)
+  {
+    switch (currentDirection)
+    {
+      case ISNORTH:
+        return ISEAST;
+      case ISEAST:
+        return ISSOUTH;
+      case ISSOUTH:
+        return ISWEST;
+      case ISWEST:
+        return ISNORTH;
+    }
+  } 
+  else//turn around
+  {
+    switch (currentDirection)
+    {
+    case ISNORTH:
+      return ISSOUTH;
+    case ISEAST:
+      return ISWEST;
+    case ISSOUTH:
+      return ISNORTH;
+    case ISWEST:
+      return ISEAST;
+    }
+  }
+}
+
+byte getBestDirection()
+{
+  //if the cell ahead of the robot is open and the distance is shorter than
+  //the current distance, and hasn't been explored go there
+  if (!(currentCell->data & currentDirection) && currentCell->distance > getCellDistance(currentDirection) && !getIsCellExplored(currentDirection))
+  {
+    return currentDirection;
+  }
+
+  //if the cell to the right is open and the distance is shorter than the current distance, and hasn't been explored go there
+  if (!(currentCell->data & getDirection(RIGHT)) && currentCell->distance > getCellDistance(getDirection(RIGHT)) && !getIsCellExplored(currentDirection))
+  {
+    return getDirection(RIGHT);
+  }
+
+  //if the cell to the left is open and the distance is shorter than the current distance, and hasn't been explored go there
+  if (!(currentCell->data & getDirection(LEFT)) && currentCell->distance > getCellDistance(getDirection(LEFT)) && !getIsCellExplored(currentDirection))
+  {
+    return getDirection(LEFT);
+  }
+
+  //if the cell ahead of the robot is open and the distance is shorter than
+  //the current distance, go there
+  if (!(currentCell->data & currentDirection) && currentCell->distance > getCellDistance(currentDirection))
+  {
+    return currentDirection;
+  }
+
+  //if the cell to the right is open and the distance is shorter than the current distance, go there
+  if (!(currentCell->data & getDirection(RIGHT)) && currentCell->distance > getCellDistance(getDirection(RIGHT)))
+  {
+    return getDirection(RIGHT);
+  }
+
+  //if the cell to the left is open and the distance is shorter than the current distance, go there
+  if (!(currentCell->data & getDirection(LEFT)) && currentCell->distance > getCellDistance(getDirection(LEFT)))
+  {
+    return getDirection(LEFT);
+  }
+
+  //if the cell ahead of the robot is open go there
+  if (!(currentCell->data & currentDirection) && currentCell->distance > getCellDistance(currentDirection))
+  {
+    return currentDirection;
+  }
+
+  //if the cell to the right is open go there
+  if (!(currentCell->data & getDirection(RIGHT)) && currentCell->distance > getCellDistance(getDirection(RIGHT)))
+  {
+    return getDirection(RIGHT);
+  }
+
+  //if the cell to the left is open go there
+  if (!(currentCell->data & getDirection(LEFT)) && currentCell->distance > getCellDistance(getDirection(LEFT)))
+  {
+    return getDirection(LEFT);
+  }
+  
+  return getDirection(BEHIND);
+}
+
+bool getIsCellExplored(byte direction)
+{
+  byte x = (currentCell->position >> SHIFT);
+  byte y = (currentCell->position & Y_ONLY);
+  switch (direction)
+  {
+  case ISNORTH:
+    y++;
+    break;
+  case ISEAST:
+    x++;
+    break;
+  case ISSOUTH:
+    y--;
+    break;
+  case ISWEST:
+    x--;
+    break;
+  }
+  return maze[x][y].data & ISEXPLORED;
+}
+
+byte getCellDistance(byte direction)
+{
+  byte x = (currentCell->position >> SHIFT);
+  byte y = (currentCell->position & Y_ONLY);
+  switch (direction)
+  {
+    case ISNORTH:
+      y++;
+      if (y == MAZE_LENGTH)
+      {
+        return USELESS;
+      }
+      break;
+    case ISEAST:
+      x++;
+      if (x == MAZE_LENGTH)
+      {
+        return USELESS;
+      }
+      break;
+    case ISSOUTH:
+      if (y == 0)
+      {
+        return USELESS;
+      }
+      y--;
+      break;
+    case ISWEST:
+      if (x == 0)
+      {
+        return USELESS;
+      }
+      x--;
+      break;
+  }
+  return maze[x][y].distance;
 }
